4ebed76d3953d6b59f279616e1be81a4e9aa7e27
upncursos <upncursos@gmail.com>
2016-03-01 16:35:33 -0500

    Proyecto polinomio adicionado

---
+++ Proyecto_Polinomio/src/proyecto_polinomio/Polinomio.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_polinomio;
+
+/**
+ *
+ * @author user
+ */
+public class Polinomio {
+    double a;
+    double b;
+    double c;
+
+    @Override
+    public String toString() {
+        
+        return  a + "x^2 + " + b + "x +" + c;
+    }
+    public double evaluarEn (double x)
+    {
+        double resultado;
+        resultado = this.a*Math.pow(x,2) + this.b*Math.pow(x, 1) + this.c;
+        return resultado;
+    }
+    
+           
+    
+    
+    
+}
---
+++ Proyecto_Polinomio/src/proyecto_polinomio/Proyecto_Polinomio.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_polinomio;
+
+/**
+ *
+ * @author user
+ */
+public class Proyecto_Polinomio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Polinomio p, p2, y;
+        double z;
+        double w=3;
+        p = new Polinomio();
+        p2 = new Polinomio();
+        p.a = 2;
+        p.b = 3;
+        p.c = 5;
+        p2.a = 1;
+        p2.b=2;
+        p2.c =2;
+        
+        y = p.sumar(p2);
+        
+       
+    }
+    
+}
