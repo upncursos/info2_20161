022aa15eb397a47a421a5e00dc37490869efbfba
upncursos <upncursos@gmail.com>
2016-03-03 15:18:10 -0500

    creación proyecto Fraccionario

---
+++ Proyecto_Fraccionario/src/proyecto_fraccionario/Fraccionario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_fraccionario;
+
+/**
+ *
+ * @author user
+ */
+public class Fraccionario {
+    int numerador;
+    int denominador;
+
+    @Override
+    public String toString() {
+        return  numerador + "/" + denominador;
+    }
+    
+    
+    
+    
+}
